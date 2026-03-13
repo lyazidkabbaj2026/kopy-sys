06b1a5c3861a0168988f7cb85856e7f0d5663417
Lyazid <lyazidkabbaj2026@gmai.com>
2026-03-13 12:43:41 +0000

    Commit v15

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+import { env } from "@/config/env";
 
 const prismaClientSingleton = () => {
-    return new PrismaClient();
+  const pool = new Pool({ connectionString: env.DATABASE_URL });
+  const adapter = new PrismaPg(pool as any);
+  return new PrismaClient({ adapter });
 };
 
 declare global {
-    var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
